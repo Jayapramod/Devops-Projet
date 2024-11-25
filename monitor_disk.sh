5a7a2fd70e024b09d59729f689d293fa61bddc9d
Jay <86703352+Jayapramod@users.noreply.github.com>
2024-11-25 11:51:18 +0530

    Update monitor_disk.sh

--- monitor_disk.sh
+++ monitor_disk.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+# Get the disk usage percentage for the root directory
 disk_usage=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g')
-echo "Current disk usage: ${disk_usage}%"  # Debug line
 
-if [ $disk_usage -ge 80 ]; then
-    echo "Disk usage is critically high: ${disk_usage}% on $(hostname)" | mail -s "Disk Utilization Alert - ${disk_usage}%" jayapramodmanikantan@gmail.com
-    echo "Email alert sent successfully"  # Debug line
-else
-    echo "Disk usage is under control: ${disk_usage}%"
+# Check if the disk usage is greater than or equal to the threshold set in Jenkins
+if [ $disk_usage -ge $DISK_THRESHOLD ]; then
+    # Send an email if the disk usage is above the threshold
+    echo "Disk usage is critically high: ${disk_usage}% on $(hostname)" | mail -s "Disk Utilization Alert - ${disk_usage}%" $ALERT_EMAIL
 fi
